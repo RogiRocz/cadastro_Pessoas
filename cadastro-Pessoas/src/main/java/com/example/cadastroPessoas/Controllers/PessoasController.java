b24de502f89ec5cda131bc157b01c9e7ee601825
RogiRocz <rogirocz@gmail.com>
2021-11-02 09:51:06 -0300

    add put router

--- cadastro-Pessoas/src/main/java/com/example/cadastroPessoas/Controllers/PessoasController.java
+++ cadastro-Pessoas/src/main/java/com/example/cadastroPessoas/Controllers/PessoasController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +34,10 @@ public class PessoasController {
     @DeleteMapping(path = "{id}")
     public void delete(@PathVariable("id") Long id) {
         pessoasService.delete(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public void update(@PathVariable("id") Long id) {
+
     }
 }
